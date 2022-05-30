220a9dda4b5eacabafa8590f261c0598861b9ba9
Vera Kutyavina <vera.kutiavina@flatstack.com>
2022-05-31 02:19:47 +0300

    fix space

--- components/pages/menu/components/MenuItem/MenuItem.tsx
+++ components/pages/menu/components/MenuItem/MenuItem.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ProductType from 'types/product';
 import { useCreateOrder } from 'lib/apollo/order';
 import { useCurrentProfile } from 'lib/apollo/currentProfile';
